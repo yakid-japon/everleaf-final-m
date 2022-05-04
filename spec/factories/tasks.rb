d3b50a3269dddf793ebe32cc2f6f122352ab6e8a
Chabla <chabijeanbaptiste@gmail.com>
2022-05-04 07:13:28 +0000

    step2

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,6 +1,10 @@
 FactoryBot.define do
     factory :task do
-        name { 'title' }
+        name { 'test_name' }
         content { 'test_content' }
+    end
+    factory :second_task, class: Task do
+        name { 'test_name_second' }
+        content { 'test_content_second' }
     end
 end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -3,6 +3,7 @@ RSpec.describe 'Fonction de gestion des tâches', type: :system do
   before do
     # あらかじめタスク一覧のtestで使用するためのタスクを二つ作成する
     FactoryBot.create(:task)
+    FactoryBot.create(:second_task)
   end
  
   describe "Nouvelle fonction de création" do
@@ -11,12 +12,20 @@ RSpec.describe 'Fonction de gestion des tâches', type: :system do
         task = FactoryBot.create(:task, name: 'task')
         visit tasks_path
         expect(page).to have_content 'task'
+        expect(page).to have_content 'test_content'
       end
     end
   end
   describe "Fonction d'affichage de liste" do
     context "Lors de la transition vers l'écran de liste" do
       it "La liste des tâches créées s'affiche" do
+      end
+    end
+    context "Lorsque les tâches sont organisées par ordre décroissant de date et d'heure de création" do
+      it "La nouvelle tâche s'affiche en haut" do
+        visit tasks_path
+        task_list = all('.task_row')
+        expect(task_list[0]).to have_content 'test_name_second'
       end
     end
   end
